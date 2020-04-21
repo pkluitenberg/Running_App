82b146c8b50fd4b078db27eaf2446a9099ed2553
pkluitenberg <paul.kluitenberg@gmail.com>
2020-04-20 22:28:46 -0500

    PK: added in script to deploy spp on ShinyApps.io

---
+++ deploy.R
@@ -0,0 +1,11 @@
+###################################################################################
+# Title: deploy.R                                                                 #
+# Purpose: this script deploys the shiny app to shinyapps.io                      #
+# Author: Paul Kluitenberg                                                        #
+# Last Modified: 2020-04-20                                                       #
+###################################################################################
+
+# begin import packages
+suppressMessages(suppressWarnings(library(rsconnect)))
+# end import packages
+
